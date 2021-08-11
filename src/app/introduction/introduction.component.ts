0ea62a78845ef3def386939f148b95a19e9ddfa8
Mogyuchi <69337624+Mogyuchi@users.noreply.github.com>
2021-08-11 12:23:06 +0900

    ✨ Add Introduction page

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-introduction',
   templateUrl: './introduction.component.html',
-  styleUrls: ['./introduction.component.sass']
+  styleUrls: ['./introduction.component.sass'],
 })
 export class IntroductionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
