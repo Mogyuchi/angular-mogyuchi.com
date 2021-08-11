c693614eeaf54074dd17879b578bda7d605a63c5
Mogyuchi <69337624+Mogyuchi@users.noreply.github.com>
2021-08-12 03:03:15 +0900

    ✨ Add randomfacts routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { IntroductionComponent } from './introduction/introduction.component';
+import { RandomfactsComponent } from './randomfacts/randomfacts.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'introduction',
     component: IntroductionComponent,
+  },
+  {
+    path: 'randomfacts',
+    component: RandomfactsComponent,
   },
 ];
 
