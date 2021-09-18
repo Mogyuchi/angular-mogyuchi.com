b3cce07e9083641bd19d4bcd41abb65c5c1ecb3f
Mogyuchi <69337624+Mogyuchi@users.noreply.github.com>
2021-09-18 10:26:12 +0900

    ✨ Add route animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RouterOutlet, ROUTER_CONFIGURATION } from '@angular/router';
+import { slideInAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
+  animations: [slideInAnimation],
 })
 export class AppComponent {
   title = 'もぎゅち';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return (
+      outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation
+    );
+  }
 }
