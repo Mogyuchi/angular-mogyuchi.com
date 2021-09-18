2712d6526611fc6e40be406f4cc66c3dda544c56
Mogyuchi <69337624+Mogyuchi@users.noreply.github.com>
2021-09-19 08:25:06 +0900

    🐛 直書きしないとダメ？

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,36 +6,7 @@ import { Meta } from '@angular/platform-browser';
   styleUrls: ['./home.component.sass'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private metaService: Meta) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.metaService.addTag({
-      name: 'description',
-      content: 'Official website of Mogyuchi.',
-    });
-    this.metaService.addTag({ property: 'og:title', content: 'Mogyuchi' });
-    this.metaService.addTag({ property: 'og:locale', content: 'ja_JP' });
-    this.metaService.addTag({
-      property: 'og:description',
-      content: 'Official website of Mogyuchi.',
-    });
-    this.metaService.addTag({ property: 'og:type', content: 'website' });
-    this.metaService.addTag({
-      property: 'og:url',
-      content: 'https://mogyuchi.com',
-    });
-    this.metaService.addTag({
-      property: 'og:image',
-      content: 'https://mogyuchi.com/assets/ogp.jpg',
-    });
-    this.metaService.addTag({
-      property: 'twitter:card',
-      content: 'summary_large_image',
-    });
-    this.metaService.addTag({ property: 'twitter:site', content: '@Mogyuchi' });
-    this.metaService.addTag({
-      property: 'twitter:creator',
-      content: '@Mogyuchi',
-    });
-  }
+  ngOnInit() {}
 }
